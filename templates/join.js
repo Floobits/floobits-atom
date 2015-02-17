d8de8a5e10a5d5bc7107e660c56f4e8f6d84ec98
Matt Kaniaris <mkaniaris@gmail.com>
2015-02-16 17:03:38 -0800

    jsx support

---
+++ templates/join.js
@@ -0,0 +1,15 @@
+/** @jsx React.DOM */
+
+var React = require('react-atom-fork');
+
+var Component = React.createClass({
+  render: function () {
+    return (
+      <div>
+      asdf
+      </div>
+    );
+  }
+});
+
+module.exports = Component;
