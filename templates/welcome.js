479db3b38b86bfb83dc10fa86b66a17fc43747cb
Matt Kaniaris <mkaniaris@gmail.com>
2015-03-11 13:39:45 -0700

    start of a welcome screen:
    Lesshat isn't supported nor is bootstrap :(

---
+++ templates/welcome.js
@@ -0,0 +1,81 @@
+/** @jsx React.DOM */
+
+"use strict";
+const React = require('react-atom-fork');
+const flux = require('flukes');
+const modal = require("../modal");
+const $ = require('atom-space-pen-views').$;
+const perms = require("../common/permission_model");
+const editorAction = require("../common/editor_action");
+const utils = require("../utils");
+
+// const ANONYMOUS_PNG = "/static/images/anonymous.png";
+const ANONYMOUS_PNG = "atom://floobits/resources/anonymous.png";
+const that = this;
+const Welcome = React.createClass({
+  render: function () {
+    var connection = this.props.connection;
+
+    return (
+      <div id="floo-welcome-wrapper">
+        
+        <div id="floo-main">
+          <div className="floo-home-slider" style={{}}>
+            <div className="container">
+              <div className="row">
+                <div className="col-md-6">
+                  <div className="hero">
+                    <h1>Floobits</h1>
+                    <h2>Write code together, anywhere.</h2>
+                  </div>
+                  <div className="hero-info">
+                    Floobits lets you collaboratively edit files in real time across text editors.
+                    We currently support Sublime Text 3, NeoVim, Emacs, Intellij, Atom, and the web.
+                  </div>
+                </div>
+                <div className="col-md-6 signup-form-container">
+                  <form className="signup-form" action="/signup" method="post">
+                    <span className="signup-title">Let’s Start!</span>
+                    <div className="signup-input-container">
+                      <div>
+                        <span className="signup-username-icon signup-icon">&nbsp;</span>
+                        <input className="signup-input" type="text" name="signup-username" placeholder="Username" tabIndex="1" />
+                      </div>
+                    </div>
+                    <div className="signup-input-container">
+                      <div>
+                        <span className="signup-email-icon signup-icon">&nbsp;</span>
+                        <input className="signup-input" type="text" name="signup-email" placeholder="Email" tabIndex="2" />
+                      </div>
+                    </div>
+                    <div className="signup-input-container">
+                      <div>
+                        <span className="signup-password-icon signup-icon">&nbsp;</span>
+                        <input className="signup-input" type="password" name="signup-password1" placeholder="Password" tabIndex="3" />
+                      </div>
+                    </div>
+                    <div className="signup-input-container">
+                      <div>
+                        <span className="signup-password-conf-icon signup-icon">&nbsp;</span>
+                        <input className="signup-input" type="password" name="signup-password2" tabIndex="4" placeholder="Password confirmation" />
+                      </div>
+                    </div>
+                    <button className="signup-btn" tabIndex="5">Sign up</button>
+                  </form>
+                  <div className="signup-form">
+                    <div className="signup-github">Or
+                      <a href="/login/github">Sign up with GitHub</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    );
+  }
+});
+
+module.exports = Welcome;
