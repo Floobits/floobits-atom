ba3006d9df2db2e758efae4a7a4d01cfc45e229b
Matt Kaniaris <mkaniaris@gmail.com>
2015-03-17 18:22:23 -0700

    adds support for logging in via new api and storing credentials.

--- templates/welcome.js
+++ templates/welcome.js
@@ -2,18 +2,82 @@
 
 "use strict";
 const React = require('react-atom-fork');
+const _ = require("lodash");
 const flux = require('flukes');
 const modal = require("../modal");
 const $ = require('atom-space-pen-views').$;
 const perms = require("../common/permission_model");
 const editorAction = require("../common/editor_action");
 const utils = require("../utils");
+const constants = require("../common/constants");
+const floorc = require("../common/floorc");
+const api = require("../common/api");
+const message_action = require("../common/message_action");
+
 
 // const ANONYMOUS_PNG = "/static/images/anonymous.png";
 const ANONYMOUS_PNG = "atom://floobits/resources/anonymous.png";
 const that = this;
 
+const Mixin = {
+  valueForRef: function (refName) {
+    return this.refs[refName].getDOMNode().value.trim();
+  }
+};
+
 const LoginForm = React.createClass({
+  getInitialState: function () {
+    return {
+      error: null
+    };
+  },
+  mixins: [Mixin],
+  onSubmit_: function (event) {
+    event.preventDefault();
+    const username = this.valueForRef("username");
+    const password = this.valueForRef("password1");
+    const host = constants.HOST;
+    const that = this;
+    api.get_credentials(host, username, password, function (err, res, body) {
+      if (res.statusCode === 401) {
+        that.setState({
+          error: "Invalid username or password",
+        });
+        return;
+      }
+      if (res.statusCode === 429) {
+        that.setState({
+          error: err,
+        });
+        return;
+      }
+      if (err) {
+        console.error(err);
+        return message_action.error(err);
+      }
+      if (!_.has(floorc, "auth")) {
+        floorc.auth = {};
+      }
+      if (!_.has(floorc.auth, host)) {
+        floorc.auth[host] = {};
+      }
+      const auth = floorc.auth[host];
+      if (auth.username && auth.username !== username) {
+        console.error("username changed?", auth.username, username);
+      }
+      auth.username = username;
+      auth.api_key = res.api_key;
+      auth.secret = res.secret;
+      auth.updated_at = res.updated_at;
+      auth.created_at = res.created_at;
+      auth.auto_created = res.auto_created;
+      try {
+        floorc.__write();
+      } catch (e) {
+        return message_action.error(err);
+      }
+    });
+  },
   componentDidMount: function () {
     const node = this.refs.username.getDOMNode();
     node.focus();
@@ -21,6 +85,11 @@ const LoginForm = React.createClass({
   render: function () {
     return <form className="signup-form" onSubmit={this.onSubmit_}>
       <span className="signup-title">Login</span>
+      { this.state.error &&  
+        <div className="signup-input-container" style={{color: "#d00303", padding: 20}}>
+          {this.state.error}
+        </div>
+      }
       <div className="signup-input-container">
         <div>
           <span className="signup-username-icon signup-icon">&nbsp;</span>
@@ -39,11 +108,9 @@ const LoginForm = React.createClass({
 });
 
 const CreateAccountForm = React.createClass({
+  mixins: [Mixin],
   errorForRef: function (ref, error) {
 
-  },
-  valueForRef: function (refName) {
-    return this.refs[refName].getDOMNode().value.trim();
   },
   onSubmit_: function (event) {
     event.preventDefault();
