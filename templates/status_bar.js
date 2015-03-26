575a97603285891de20df276756f3737860fdcbd
Bjorn Tipling <bjorn@ambientchill.com>
2015-03-26 15:56:15 -0700

    Status bar. #28

---
+++ templates/status_bar.js
@@ -0,0 +1,16 @@
+/** @jsx React.DOM */
+
+"use strict";
+const React = require('react-atom-fork');
+
+module.exports = React.createClass({
+  render: function() {
+
+
+    return (
+      <div className="floobits-status-bar">
+      Connected to {this.props.owner}/{this.props.workspace} as {this.props.username}.
+      </div>
+    );
+  }
+});
