8b310885df64aab94ecf7fbb6ceff8eb4a278444
Bjorn Tipling <bjorn@ambientchill.com>
2015-03-27 14:09:35 -0700

    Add tab indexes.

--- templates/handle_request_perm.js
+++ templates/handle_request_perm.js
@@ -33,9 +33,9 @@ module.exports = React.createClass({
         <div className="well">
           <div className="row">
             <div className="col-lg-12 btn-group">
-              <button className="btn btn-warning" onClick={this.grant}>Grant Access</button>
-              <button className="btn btn-primary" onClick={this.deny}>Deny Access</button>
-              <button className="btn btn-default" onClick={this.destroy}>Ignore</button>
+              <button tabIndex="0" className="btn btn-warning" onClick={this.grant}>Grant Access</button>
+              <button tabIndex="10" className="btn btn-primary" onClick={this.deny}>Deny Access</button>
+              <button tabIndex="20" className="btn btn-default" onClick={this.destroy}>Ignore</button>
             </div>
           </div>
         </div>
