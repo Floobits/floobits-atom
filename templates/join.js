9356068e8d0643082d199c2f3741eabb2e54be27
Matt Kaniaris <mkaniaris@gmail.com>
2015-03-27 14:05:18 -0700

    Fix tabbing in Join Form and use mixins

--- templates/join.js
+++ templates/join.js
@@ -4,11 +4,14 @@
 
 const $ = require('atom-space-pen-views').$;
 const React = require('react-atom-fork');
+const mixins = require("./mixins");
 
 const JoinWorkspace = React.createClass({
+  mixins: [mixins.ReactUnwrapper, mixins.FormMixin],
   getInitialState: function () {
     return {
-      path: ""
+      path: "",
+      url: this.props.url,
     };
   },
   onSubmit: function () {
@@ -25,23 +28,6 @@ const JoinWorkspace = React.createClass({
     const path = files[0].path;
     this.setState({path: path});
   },
-  tabs: ["url", "dir", "cancel", "submit"],
-  onTab: function (event) {
-    event.preventDefault();
-    const position = parseInt(event.target.attributes["data-tab_order"].value, 10);
-    const direction = event.shiftKey ? - 1 : 1;
-    let next = position + direction;
-    // no negative indexing in JS :(
-    if (next < 0) {
-      next = this.tabs.length - 1;
-    } else if (next >= this.tabs.length) {
-      next = 0;
-    }
-
-    const refName = this.tabs[next];
-    const ref = this.refs[refName].getDOMNode();
-    ref.focus();
-  },
   componentDidUpdate: function () {
     $("#ultra-secret-hidden-file-input").attr("webkitdirectory", true);
   },
@@ -52,22 +38,7 @@ const JoinWorkspace = React.createClass({
       $("#floobits-url").focus();
     }, 0);
 
-    const that = this;
-    $("#join-workspace").keydown(function(e) {
-      switch (e.keyCode) {
-        case 27:  // escape
-          that.destroy();
-          return;
-        case 13:  // enter
-          that.onSubmit();
-          return;
-        case 9:  // tab
-          that.onTab(e);
-          return;
-        default:
-          break;
-      }
-    });
+    mixins.FormMixin.componentDidMount.call(this);
 
     const root = atom.project.rootDirectories;
     if (!root.length) {
@@ -80,10 +51,6 @@ const JoinWorkspace = React.createClass({
     this.setState({path: path});
     $("#ultra-secret-hidden-file-input").attr("webkitdirectory", true);
   },
-  destroy: function () {
-    $("#join-workspace").off("keydown");
-    this.getDOMNode().parentNode.destroy(); 
-  },
   onTyping: function (event) {
     const path = event.target.value;
     if (path === this.state.path) {
@@ -93,6 +60,9 @@ const JoinWorkspace = React.createClass({
   },
   focusFileInput: function (event) {
     $("#ultra-secret-hidden-file-input").trigger('click');
+  },
+  updateURL: function (e) {
+    this.setState({url: e.target.value});
   },
   render: function () {
     return (
@@ -106,7 +76,7 @@ const JoinWorkspace = React.createClass({
               <div className="col-lg-12">
                 <div className="input-group">
                   <span className="input-group-addon" id="url-addon">URL</span>
-                  <input data-tab_order="0" id="floobits-url" ref="url" className="native-key-bindings form-control" value={this.props.url} aria-describedby="url-addon" />
+                  <input tabIndex="1" id="floobits-url" ref="url" className="native-key-bindings form-control" onChange={this.updateURL} value={this.state.url} aria-describedby="url-addon" />
                 </div>
               </div>
             </div>
@@ -115,7 +85,7 @@ const JoinWorkspace = React.createClass({
               <div className="col-lg-12">
                 <div className="input-group">
                   <span onClick={this.clickFileInput} className="input-group-addon" id="directory-addon" >Select Directory</span>
-                  <input data-tab_order="1" onFocus={this.focusFileInput} onChange={this.onTyping} type="text" className="native-key-bindings form-control" placeholder="..." 
+                  <input tabIndex="3" onFocus={this.focusFileInput} onChange={this.onTyping} type="text" className="native-key-bindings form-control" placeholder="..." 
                     aria-describedby="directory-addon" value={this.state.path} ref="dir" />
                 </div>
               </div>
@@ -123,8 +93,8 @@ const JoinWorkspace = React.createClass({
 
             <div className="row">
               <div className="col-lg-6">
-                <input data-tab_order="3" ref="submit" onClick={this.onSubmit} type="submit" value="Join" className="floobits-submit" />
-                <input data-tab_order="2" ref="cancel" onClick={this.destroy} type="submit" value="Cancel" className="floobits-submit" />
+                <input tabIndex="5" ref="submit" onClick={this.onSubmit} type="submit" value="Join" className="floobits-submit" />
+                <input tabIndex="7" ref="cancel" onClick={this.destroy} type="submit" value="Cancel" className="floobits-submit" />
               </div>
             </div>
           </form>
