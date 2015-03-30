9bb5939b2e15cd6bc389c7ed514add89e7ee78ff
Matt Kaniaris <mkaniaris@gmail.com>
2015-03-30 15:51:06 -0700

    add header and close button to user list pane

---
+++ templates/user_list_pane.js
@@ -0,0 +1,24 @@
+/** @jsx React.DOM */
+
+"use babel";
+"use strict";
+
+const React = require('react-atom-fork');
+const _ = require("lodash");
+const mixins = require("./mixins");
+const UserlistView = require("./user_view").UserlistView;
+
+module.exports = React.createClass({
+  mixins: [mixins.ReactUnwrapper],
+  render: function () {
+    return (
+      <div id="user-list-pane">
+        <div id="user-list-pane-header">
+          <img src="atom://floobits/resources/icon_64x64.png" />Floobits Users List
+          <span className="pull-right" onClick={this.destroy}>x</span>
+        </div>
+        <UserlistView users={this.props.users} me={this.props.me} prefs={this.props.prefs} />
+      </div>
+    );
+  }
+});
