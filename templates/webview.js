ed6a223813768ee4089f69428f5e98881e15524a
Matt Kaniaris <mkaniaris@gmail.com>
2015-04-20 18:46:51 -0700

    hooking up webview events

--- templates/webview.js
+++ templates/webview.js
@@ -5,19 +5,37 @@
 const Proto = Object.create(HTMLElement.prototype);
 
 Proto.createdCallback = function () {
-  this.component = document.createElement("webview");
-  this.component.allowtransparency = "on";
+  this.webview = document.createElement("webview");
+  this.webview.allowtransparency = "on";
+};
+
+Proto.load = function (src) {
+  this.webview.src = src;
 };
 
 Proto.attachedCallback = function () {
-  this.appendChild(this.component);
+  const webview = this.webview;
+  this.appendChild(this.webview);
+
+  webview.addEventListener("console-message", function(e) {
+    console.log("logged a message: ", e.level, e.message);
+  });
+  webview.addEventListener("did-finish-load", function () {
+    console.log("finished loading", webview.getUrl());
+  });
+  webview.addEventListener("new-window", function (e) {
+    console.log("new window", e.url);
+  });
+  webview.addEventListener("message", function (e) {
+    console.log("message", e.origin, e.message);
+  });
 };
 
 Proto.detachedCallback = function () {
-  if (!this.component) {
+  if (!this.webview) {
     return;
   }
-  this.component.stop();
+  this.webview.terminate();
 };
 
 Proto.onDestroy = function (pane) {
