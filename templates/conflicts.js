4869e25d933b9fe91e1a5cfe8aa5a1c2f87168a5
Matt Kaniaris <mkaniaris@gmail.com>
2015-04-27 16:17:46 -0700

    obey disconnects

--- templates/conflicts.js
+++ templates/conflicts.js
@@ -117,7 +117,7 @@ module.exports = React.createClass({
     const newFiles = this.render_("Uploading: ", this.props.newFiles);
 
     return (<div>
-      <h1>You just created {fl.floourl ? fl.floourl.toString() : "the workspace"}.</h1>
+      <h1 className="native-key-bindings">You just created {fl.floourl ? fl.floourl.toString() : "the workspace"}.</h1>
       { newFiles }
     </div>);
   },
