34add6832a8447c6cf9d5d0b4b02bb10087a897c
Geoff Greer <geoff@greer.fm>
2015-08-24 17:32:07 -0700

    Hide dumb debugging context menu thing. Use api key or username when authing. Update status bar.

--- templates/status_bar.js
+++ templates/status_bar.js
@@ -1,13 +1,31 @@
 /** @jsx React.DOM */
-
 "use strict";
-const React = require('react-atom-fork');
+
+const React = require("react-atom-fork");
+const flux = require("flukes");
+
+const floop = require("../common/floop");
 
 module.exports = React.createClass({
+  mixins: [flux.createAutoBinder(["me"])],
+  getInitialState: function () {
+    return {
+      conn_status: "Connecting..",
+    }
+  },
+  componentWillMount: function () {
+    const that = this;
+    floop.onROOM_INFO(function () {
+      that.setState({conn_status: "Connected"});
+    });
+    floop.onDISCONNECT(function () {
+      that.setState({conn_status: "Disconnected"});
+    });
+  },
   render: function () {
     return (
       <div className="floobits-status-bar">
-        Connected to {this.props.owner}/{this.props.workspace} as {this.props.username}.
+        {this.props.me.id}@{this.props.floourl.owner}/{this.props.floourl.workspace}: {this.state.conn_status}.
       </div>
     );
   }
