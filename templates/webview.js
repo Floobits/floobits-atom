da439b49005539490fbca1c794f5e5dd251f22c5
Matt Kaniaris <mkaniaris@gmail.com>
2015-09-10 17:05:16 -0700

    restore native keybindings to our webview (tab mainly)

--- templates/webview.js
+++ templates/webview.js
@@ -24,17 +24,17 @@ Proto.createdCallback = function () {
     let m = e.message;
     const prefix = "::atom-floobits-ipc::";
     if (!m.startsWith(prefix)) {
-      return true;
+      return;
     }
     m = m.slice(prefix.length);
     console.log(m);
     try {
       m = JSON.parse(m);
     } catch (e) {
-      return true;
+      return;
     }
     this.onCredentials(m.auth);
-    return true;
+    return;
   }.bind(this));
   this.frame = frame;
 };
@@ -68,45 +68,12 @@ Proto.load = function (host) {
   this.host = host;
   this.pane = new Pane("Floobits", "", this);
   atom.workspace.getActivePane().activateItem(this.pane);
-  this.frame.src = `https://${host}/signup/atom?atomthing=true`;
-};
-
-Proto.handle_login = function (auth) {
-  const host = _.keys(auth)[0];
-  auth = auth[host];
-
-  if (!_.has(floorc, "auth")) {
-    floorc.auth = {};
-  }
-
-  if (!_.has(floorc.auth, host)) {
-    floorc.auth[host] = {};
-  }
-  const floorc_auth = floorc.auth[host];
-  const username = auth.username;
-  if (floorc_auth.username && floorc_auth.username !== username) {
-    console.error("username changed?", auth.username, username);
-  }
-  floorc_auth.username = username;
-  floorc_auth.api_key = auth.api_key;
-  floorc_auth.secret = auth.secret;
-  try {
-    floorc.__write();
-  } catch (e) {
-    return message_action.error(e);
-  }
+  this.frame.src = `https://${host}/signup/atom`;
 };
 
 Proto.attachedCallback = function () {
+  this.className = "floobits-nativize";
   this.appendChild(this.frame);
-  const that = this;
-  window.addEventListener("message", function (e) {
-    const data = JSON.parse(e.data);
-    if (!data.auth) {
-      return;
-    }
-    that.handle_login(data.auth);
-  });
 };
 
 Proto.detachedCallback = function () {
