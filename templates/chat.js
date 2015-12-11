856ae2bc6e4b680014a90b55780b888c3969240f
Matt Kaniaris <mkaniaris@gmail.com>
2015-12-11 15:22:40 -0800

    fixed styling to match live web

--- templates/chat.js
+++ templates/chat.js
@@ -11,19 +11,35 @@ const $ = require('atom-space-pen-views').$;
 const utils = require("../utils");
 const flux = require("flukes");
 
+function prettyTime (t) {
+  const d = utils.formatDate(new Date(t));
+  return `${d.hour}:${d.minute} ${d.meridian}`;
+}
 module.exports = React.createClass({
   mixins: [flux.createAutoBinder(['msgs'])],
   render: function () {
     const msgs = this.props.msgs.map(function (msg) {
+      const userColor = utils.user_color(msg.username);
       return (
-        <div>
-          [{msg.time}] {msg.username}: {msg.data}
-        </div>);
+        <div className="message">
+          <div className="message-content">
+            <div className="message-timestamp">{prettyTime(msg.time)}</div>
+            <div className="message-text">
+              <span className="message-username">
+                <span className="user-color-square" style={{backgroundColor: userColor}}></span>
+                {msg.username}:&nbsp;
+              </span>
+              {msg.data}
+            </div>
+          </div>
+        </div>
+      );
     });
     return (
-      <div className="native-key-bindings" style={{overflow: "auto", border: 0, padding: 10, left: 0, top: 0, margin: 0, width: "100%", height: "100%"}} >
-        hello
-        {msgs}
+      <div className="native-key-bindings floobits-messages-container" style={{overflow: "auto", border: 0, padding: 10, left: 0, top: 0, margin: 0, width: "100%", height: "100%"}} >
+        <div className="messages-list">
+          {msgs}
+        </div>
       </div>
     );
   }
