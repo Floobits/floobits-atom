66b6323fc955350ea7cc0f8d16fb2cee5be53e41
Matt Kaniaris <mkaniaris@gmail.com>
2015-12-11 16:23:01 -0800

    added input so we can send messages

--- templates/chat.js
+++ templates/chat.js
@@ -2,13 +2,9 @@
 /*global fl */
 "use strict";
 
-const _ = require("lodash");
-const path = require("path");
 const React = require('react-atom-fork');
-const floop = require("../common/floop");
-const fs = require("fs");
-const $ = require('atom-space-pen-views').$;
 const utils = require("../utils");
+const floop = require("../common/floop");
 const flux = require("flukes");
 
 function prettyTime (t) {
@@ -17,6 +13,24 @@ function prettyTime (t) {
 }
 module.exports = React.createClass({
   mixins: [flux.createAutoBinder(['msgs'])],
+  handleMessage_: function (event) {
+    event.preventDefault();
+    const input = this.refs.newMessage.getDOMNode();
+    const txt = input.value;
+    input.value = "";
+    const ret = floop.send_msg({data: txt});
+    if (ret) {
+      console.error(ret);
+      return;
+    }
+    this.props.msgs.push({username: this.props.username, time: Date.now(), data: txt});
+  },
+  componentDidMount: function () {
+    this.focus();
+  },
+  focus: function () {
+    this.refs.newMessage.getDOMNode().focus();
+  },
   render: function () {
     const msgs = this.props.msgs.map(function (msg) {
       const userColor = utils.user_color(msg.username);
@@ -36,7 +50,15 @@ module.exports = React.createClass({
       );
     });
     return (
-      <div className="native-key-bindings floobits-messages-container" style={{overflow: "auto", border: 0, padding: 10, left: 0, top: 0, margin: 0, width: "100%", height: "100%"}} >
+      <div className="native-key-bindings floobits-messages-container" style={
+        {overflow: "auto", border: 0, padding: 10, left: 0, top: 0, margin: 0, width: "100%", height: "100%"}}
+        onMouseUp={this.focus}
+        >
+        <div className="chat-input-container">
+          <form onSubmit={this.handleMessage_}>
+            <input type="text" ref="newMessage" defaultValue="" className="chat-input" placeholder="type here to chat"/>
+          </form>
+        </div>
         <div className="messages-list">
           {msgs}
         </div>
